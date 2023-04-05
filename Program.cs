d7416f7f6a79c4dbc415bc02897a70fd698ff0aa
ZingaG <trad_bar@mail.ru>
2023-04-05 21:23:38 +0300

    SumQualifiedElements function added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿gi
+﻿string[] a = {"Hello", "2", "world", ":-)"};
+
+
+int SumQualifiedElements(string[] a)
+{
+    int result = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if(a[i].Length <= 3)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+System.Console.WriteLine(SumQualifiedElements(a));

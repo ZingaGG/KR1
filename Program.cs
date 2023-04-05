94ea031079a9983ea49bbd22c0598d9ecb7abff1
ZingaG <trad_bar@mail.ru>
2023-04-05 22:20:03 +0300

    ArrayEdit function added, PrintStringArray function added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿string[] a = {"Hello", "2", "world", ":-)"};
 
+int ArrayLength = SumQualifiedElements(a);
+
+string[] ResultArray = ArrayEdit(a, ArrayLength);
+
+PrintStringArray(ResultArray);
 
 int SumQualifiedElements(string[] a)
 {
@@ -14,4 +19,32 @@ int SumQualifiedElements(string[] a)
     return result;
 }
 
-System.Console.WriteLine(SumQualifiedElements(a));
+string[] ArrayEdit(string[] a, int ArrayLength)
+{
+    string[] result = new string[ArrayLength];
+    int index = 0;
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = index; j < a.Length; j++)
+        {
+            if(a[j].Length <= 3)
+            {
+                result[i] = a[j];
+                index = j+1;
+                break;
+            }
+        }
+        
+    }
+
+    return result;
+}
+
+void PrintStringArray(string[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        System.Console.WriteLine($"Array[{i}] = {a[i]}");   
+    }
+}
+
